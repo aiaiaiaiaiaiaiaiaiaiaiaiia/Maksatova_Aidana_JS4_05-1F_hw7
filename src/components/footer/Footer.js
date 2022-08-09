5ca69dbad54169e097f809a09e820006c2bb3782
aiaiaiaiaiaiaiaiaiaiaiaiia <99736790+aiaiaiaiaiaiaiaiaiaiaiaiia@users.noreply.github.com>
2022-08-09 18:08:52 +0600

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
-import classes from "./footer.module.css"
+
 
 function Footer(props) {
     return (
         <ul>
             <li>
-                <NavLink to="/"  className={classes.link}>main page</NavLink>
+                <NavLink to="/"  >main page</NavLink>
             </li>
             <li>
-                <NavLink to="/users" className={classes.link}> users page</NavLink>
+                <NavLink to="/users"> users page</NavLink>
             </li>
         </ul>
     );
