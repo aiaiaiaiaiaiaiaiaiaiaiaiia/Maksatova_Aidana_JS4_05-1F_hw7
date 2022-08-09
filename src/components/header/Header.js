28448a82271625b6d4833ce1b225f2d420a71d73
aiaiaiaiaiaiaiaiaiaiaiaiia <99736790+aiaiaiaiaiaiaiaiaiaiaiaiia@users.noreply.github.com>
2022-08-09 18:11:18 +0600

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
-import classes from "./header.module.css"
+
 
 function Header() {
     return (
         <ul>
             <li>
-                <NavLink to="/about" className={classes.linki}>about page</NavLink>
+                <NavLink to="/about">about page</NavLink>
             </li>
             <li>
-                <NavLink to="/information" className={classes.linki}>information page</NavLink>
+                <NavLink to="/information" >information page</NavLink>
             </li>
         </ul>
     );
